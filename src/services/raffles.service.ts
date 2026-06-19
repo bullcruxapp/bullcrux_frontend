135687920cacb1823eb220cd1fbea15545d34828
bullcruxapp <bullcruxapp@gmail.com>
2026-06-19 15:50:55 -0300

    Fallback API_URL to process.env.API_URL

--- src/services/raffles.service.ts
+++ src/services/raffles.service.ts
@@ -1,6 +1,6 @@
 import { Raffle } from "@/models/raffle.model";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = process.env.API_URL || process.env.NEXT_PUBLIC_API_URL;
 
 export const getAllRaffles = async () => {
 
