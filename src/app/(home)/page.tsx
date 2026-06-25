3f45af2fd5911070b9272daa2574e0fcb4d52a43
bullcruxapp <bullcruxapp@gmail.com>
2026-06-25 18:16:36 -0300

    Add files via upload

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,18 +2,18 @@
 import { getAllRaffles } from "@/services/raffles.service";
 import HomePageComponent from "./component/HomePageComponent"
 import "./homepage.css";
-import { getServerSession } from "next-auth/next";
+
+const API_URL = process.env.API_URL || process.env.NEXT_PUBLIC_API_URL;
 
 export default async function HomePage() {
-
-    const session = await getServerSession();
-
-    const [raffles] = await Promise.all([
-        getAllRaffles()
+    const [raffles, featuredRaffle] = await Promise.all([
+        getAllRaffles(),
+        fetch(`${API_URL}/raffle/featured`, { cache: 'no-store' })
+            .then(r => r.ok ? r.json() : null)
+            .catch(() => null)
     ]);
-    
+
     return (
-        <HomePageComponent raffles={raffles} />
+        <HomePageComponent raffles={raffles} featuredRaffle={featuredRaffle} />
     )
 }
-
