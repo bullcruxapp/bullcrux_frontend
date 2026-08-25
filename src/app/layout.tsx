f18d68593a12b2f902e01ca5816ce564ff22a28c
bullcruxapp <bullcruxapp@gmail.com>
2026-08-25 16:04:05 -0300

    Add files via upload

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import NavbarWrapper from "../components/NavbarWrapper";
 import SessionProviderWrapper from "../components/SessionProviderWrapper";
-import DesktopLayout from "../components/DesktopLayout/DesktopLayout";
+import AppShell from "../components/AppShell";
 
 const sfPro = localFont({
   src: "../fonts/SF-Pro.ttf",
@@ -26,18 +25,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={sfPro.variable}>
         <SessionProviderWrapper>
-          {/* Desktop layout (>= 1024px) */}
-          <DesktopLayout>
+          <AppShell>
             {children}
-          </DesktopLayout>
-
-          {/* Mobile layout (< 1024px) */}
-          <div className="mobile-only-wrapper">
-            <div className="app-container">
-              {children}
-              <NavbarWrapper />
-            </div>
-          </div>
+          </AppShell>
         </SessionProviderWrapper>
       </body>
     </html>
