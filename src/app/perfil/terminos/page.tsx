b7bee080a2e97d5d9bb9605c86ee61a934ce867f
bullcruxapp <bullcruxapp@gmail.com>
2026-09-16 12:16:03 -0300

    Add TerminosPage component for terms display

---
+++ src/app/perfil/terminos/page.tsx
@@ -0,0 +1,9 @@
+'use server'
+import TerminosComponent from "./component/TerminosComponent";
+import "./terminos.css";
+
+export default async function TerminosPage() {
+    return (
+        <TerminosComponent />
+    );
+}
